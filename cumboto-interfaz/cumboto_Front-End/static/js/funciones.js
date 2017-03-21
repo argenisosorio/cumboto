ec717276ed865e1d84bdeb2fe1b08fa395d34c11
Argenis Osorio <aosorio@cenditel.gob.ve>
2017-03-21 02:37:43 -0400

    Agregada función que hace desaparecer las notificaciones(alerts) de bootstrap.

--- cumboto-interfaz/cumboto_Front-End/static/js/funciones.js
+++ cumboto-interfaz/cumboto_Front-End/static/js/funciones.js
@@ -212,3 +212,12 @@ function passwordStrength(password) {
      document.getElementById("passwordStrength").className = "strength" + score;
      document.getElementById("passwordMeterId").value = score;
 }
+
+/**
+ * @brief Función que hace desaparecer las notificaciones(alerts) de bootstrap.
+ */
+$("document").ready(function () {
+   $(".alert-success").fadeTo(2000, 500).fadeOut(600, function(){
+     $(".alert-success").effect( 'blind' , "slow" );
+  });
+});
