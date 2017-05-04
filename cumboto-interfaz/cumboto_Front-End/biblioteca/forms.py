4a23b6c497c601b6ba1d68f4620689756aa8384f
Argenis Osorio <aosorio@cenditel.gob.ve>
2017-05-04 01:35:31 -0400

    Ordenado el cumboto-interfaz/cumboto_Front-End/biblioteca/forms.py

--- cumboto-interfaz/cumboto_Front-End/biblioteca/forms.py
+++ cumboto-interfaz/cumboto_Front-End/biblioteca/forms.py
@@ -7,26 +7,19 @@ from django.utils.translation import ugettext_lazy as _
 
 class registrar_form(ModelForm):
 
-    cargar_app = forms.FileField(
-        label=("Cargar Aplicación"),
-        widget=forms.FileInput(attrs={'class': 'filestyle',
-                                      'type': 'file',
-                                      'data-placeholder' : 'Archivos permitidos: .zip ',
-                                      'data-buttonName' : 'btn-default',
-                                      'data-toggle': 'tooltip',
-                                      'data-buttonText': 'Buscar archivo',
-                                      'data-classInput' : 'input-small',
-                                      'data-size':'sm',
-                                      'required': 'true',
-                                      'title': _("Seleccione el zip que contenga la Aplicacion a cargar"),
-
-                                   }))
-
-
+    cargar_app = forms.FileField(label=("Cargar Aplicación"),widget=forms.FileInput(attrs={
+        'class': 'filestyle',
+        'type': 'file',
+        'data-placeholder' : 'Archivos permitidos: .zip ',
+        'data-buttonName' : 'btn-default',
+        'data-toggle': 'tooltip',
+        'data-buttonText': 'Buscar archivo',
+        'data-classInput' : 'input-small',
+        'data-size':'sm',
+        'required': 'true',
+        'title': _("Seleccione el zip que contenga la Aplicacion a cargar"),
+    }))
 
     class Meta:
         model = registrar_app
         fields=['cargar_app']
-
-
-    
