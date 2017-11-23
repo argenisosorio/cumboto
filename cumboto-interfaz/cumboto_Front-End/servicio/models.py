9e0c8d4fde27923d3bc4027c8fffa599db2ef65e
jhonathan <segurajhon96@gmail.com>
2017-11-23 16:17:28 -0400

    Limpieza de plantillas y modelo en servicios

--- cumboto-interfaz/cumboto_Front-End/servicio/models.py
+++ cumboto-interfaz/cumboto_Front-End/servicio/models.py
@@ -1,45 +1,33 @@
-from django.db import models
-
-# Create your models here.
 # -*- coding: utf-8 -*-
+"""
+Sistema para transmisión de aplicaciones desde las salas de control maestro de TV (CUMBOTO)
+Copyleft (@) 2017 CENDITEL nodo Mérida - https://cumaco.cenditel.gob.ve/desarrollo/wiki/ftransporte
+"""
+## @namespace capa.models
+#
+# Contiene las clases, atributos, métodos y/o funciones a implementar para los modelos del módulo de servicios
+# @author Jhonathan Salas Segura (jsalas at cenditel.gob.ve)
+# @author <a href='http://www.cenditel.gob.ve'>Centro Nacional de Desarrollo e Investigación en Tecnologías Libres
+# (CENDITEL) nodo Mérida - Venezuela</a>
+# @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-
+# Clases Consulta de Servicios
 class servicioModel(models.Model):
+    servicio = models.CharField(help_text=("Servicios disponibles"), max_length=100, primary_key=True)    
+    ranura = models.IntegerField(help_text=("Numero de ranura"))
+    cod_servicio = models.CharField(help_text=("Codigo del servicio"), max_length=100, null=True)
+    nom_servicio = models.CharField(help_text=("Nombre del servicio"), max_length=100, null=True)
+    cod_api = models.CharField(help_text=("Codigo de la aplicacion"), max_length=100)
+    nom_api = models.CharField(help_text=("Nombre de la aplicacion"), max_length=100)
+    modalidad = models.CharField(help_text=("Modalidad en que se encuentra la aplicacion"), max_length=15, null=True)
 
-    servicio = models.CharField(
-        max_length=12, help_text=_("Servicios disponible"),primary_key=True
-    )
-
-    ranura_int = models.IntegerField(
-        help_text=_("Numero de ranura"),
-    )
-
-    ranura_str = models.CharField(
-        max_length=12, help_text=_("Ranura str"),
-    )
-
-    codigo_int = models.CharField(
-        max_length=12, help_text=_("Codigo de la aplicacion"),
-    )
-
-    nombre = models.CharField(
-    max_length=12, help_text=_("Nombre de la aplicacion"),
-    )
-
-    modalidad_int = models.CharField(
-        max_length=12, help_text=_("Modalidad en la que se encuentra la aplicacion"),
-    )
-
-    modalidad_str = models.CharField(
-        max_length=12, help_text=_("Modalidad en la que se encuentra la aplicacion"),
-    )
-
-class DataModelo(models.Model):
-
-    ranura = models.IntegerField(verbose_name="full name")
-    codigo_app = models.CharField(verbose_name="full name", max_length=100)
-    modalidad = models.CharField(verbose_name="full name", max_length=100)
+# Clases Configurar Servicio Emergente
+class DataModel(models.Model):
+    ranura = models.IntegerField(help_text=("Numero de ranura"))
+    codigo_app = models.CharField(help_text=("Codigo de la aplicacion"), max_length=100)
+    modalidad = models.CharField(help_text=("Modalidad en que se encuentra la aplicacion"), max_length=15, null=True)
